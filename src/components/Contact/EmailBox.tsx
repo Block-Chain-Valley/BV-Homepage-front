5d020f823f2daefa7eb6c93bb94ca37499cd8fb9
mangming2 <leejiho35@gmail.com>
2023-05-04 22:00:56 +0900

    Update EmailBox.tsx

--- src/components/Contact/EmailBox.tsx
+++ src/components/Contact/EmailBox.tsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 
 const EmailBox = () => {
-  const form = React.useRef(null);
+  const form = useRef<HTMLFormElement>(null);
 
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -17,22 +17,28 @@ const EmailBox = () => {
       .then(
         (result) => {
           console.log(result.text);
+          alert("메일이 성공적으로 전송되었습니다.");
         },
         (error) => {
           console.log(error.text);
+          alert("메일 전송에 실패하였습니다.");
         }
       );
+
+    if (form.current) {
+      form.current.reset();
+    }
   };
 
   return (
     <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" name="user_name" />
+      <label htmlFor="email">Email</label>
+      <input type="email" id="email" name="user_email" />
+      <label htmlFor="message">Message</label>
+      <textarea id="message" name="message" />
+      <input type="submit" name="Send" value="Send" />
     </form>
   );
 };
