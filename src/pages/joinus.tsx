62996ef271860faddf243872b45008d163fb25c1
mangming2 <leejiho35@gmail.com>
2023-05-06 15:05:12 +0900

    Update joinus.tsx

--- src/pages/joinus.tsx
+++ src/pages/joinus.tsx
@@ -1,8 +1,13 @@
 import { useEffect, useRef, useState } from "react";
 import JoinAnimation from "@/components/Joinus/JoinAnimation";
+import { SEO } from "@/seo/SEO";
 const joinus = () => {
   return (
     <div>
+      <SEO
+        title="입사 지원 페이지"
+        description="당사에 입사 지원을 할 수 있는 페이지입니다."
+      />
       <JoinAnimation />
     </div>
   );
