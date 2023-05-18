439b36c6d9baf3988df07066dc2b3feea4549529
mangming2 <leejiho35@gmail.com>
2023-05-18 22:20:38 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,6 +57,34 @@ const dummyposts = [
   },
 ];
 
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <svg
+        className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-500"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+      <span>Loading...</span>
+    </div>
+  );
+};
+
 const PostsPage = ({ posts }: { posts: any }) => {
   return (
     <div>
@@ -80,8 +108,8 @@ export default function Home() {
 
   const getPosts = async () => {
     try {
-      // const posts = await mediumAPI.getPosts();
-      const posts = dummyposts;
+      const posts = await mediumAPI.getPosts();
+      //const posts = dummyposts;
       setPost(posts);
       setPostImages(posts.map((post) => post.imgSrc));
       setPostTitles(posts.map((post) => post.title));
@@ -122,11 +150,15 @@ export default function Home() {
       <div>
         <div className="text-black text-3xl p-4">Medium</div>
         <div className="p-10">
-          <DetailCarousel
-            images={postImages}
-            title={postTitles}
-            url={postUrls}
-          />
+          {post.length === 0 ? (
+            <Loading />
+          ) : (
+            <DetailCarousel
+              images={postImages}
+              title={postTitles}
+              url={postUrls}
+            />
+          )}
         </div>
       </div>
     </main>
